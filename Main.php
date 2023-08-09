3cc3fb06ec7f31f83aeefbab8a6b31ae590b8a3b
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-09 15:31:25 +1000

    Add files via upload

---
+++ Main.php
@@ -0,0 +1,50 @@
+<?php
+/* 
+//         /$$                                     /$$$$$$   /$$$$$$  /$$          
+          | $$                                    /$$$_  $$ /$$__  $$| $$          
+  /$$$$$$$| $$   /$$ /$$   /$$  /$$$$$$$ /$$$$$$$| $$$$\ $$| $$  \__/| $$ /$$   /$$
+ /$$_____/| $$  /$$/| $$  | $$ /$$_____//$$_____/| $$ $$ $$| $$$$    | $$| $$  | $$
+|  $$$$$$ | $$$$$$/ | $$  | $$|  $$$$$$|  $$$$$$ | $$\ $$$$| $$_/    | $$| $$  | $$
+ \____  $$| $$_  $$ | $$  | $$ \____  $$\____  $$| $$ \ $$$| $$      | $$| $$  | $$
+ /$$$$$$$/| $$ \  $$|  $$$$$$$ /$$$$$$$//$$$$$$$/|  $$$$$$/| $$      | $$|  $$$$$$$
+|_______/ |__/  \__/ \____  $$|_______/|_______/  \______/ |__/      |__/ \____  $$
+                     /$$  | $$                                            /$$  | $$
+                    |  $$$$$$/                                           |  $$$$$$/
+                     \______/                                             \______/ 
+@author: skyss0fly: https://github.com/skyss0fly
+@repository: https://www.github.com/skyss0fly/AntiSpaces
+@License: GPL 3.0
+- This plugin was created by skyss0fly inspired by: https://poggit.pmmp.io/p/NoSpace by (Wildan-Dev461)[https://github.com/Wildan-Dev461] -
+
+*/
+namespace skyss0fly\AntiSpaces;
+
+use pocketmine\player\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+
+public function onLoad(): void {
+	$this->registerEvents($this, $this);
+$this->saveDefaultConfig();
+$cfg = $this->getConfig();
+$enabled = $cfg->get("Enabled");
+if ($enabled){
+$this->getLogger()->info("AntiSpaces Enabled");
+}
+else {
+$this->getLogger()->info("AntiSpaces is not enabled in Config\nDisabling:(");
+$this->disable();
+}
+
+}
+public function onJoin($event Listener, $player Player): void {
+$playername = $this->getPlayer()->getPlayerName();
+
+if (strpos($playername , " ")) {
+	$new_str = str_replace(' ', '_', $playername);
+$this->getPlayer()->setDisplayName($new_str);
+}
+}
