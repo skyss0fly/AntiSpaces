f07c3be30a8b1cc7bdd97febaade828b21a40b70
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-09 16:12:53 +1000

    Update Main.php

--- src/skyss0fly/AntiSpaces/Main.php
+++ src/skyss0fly/AntiSpaces/Main.php
@@ -37,7 +37,7 @@ $this->getLogger()->info("AntiSpaces Enabled");
 }
 else {
 $this->getLogger()->info("AntiSpaces is not enabled in Config\nDisabling:(");
-
+$this->getServer()->getPluginManager()->disablePlugin($this);
 }
 }
 
